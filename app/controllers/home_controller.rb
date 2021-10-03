e2f99ef123682ee1b846cc3ae96477819c2b2c09
Andrew <kolbe1129@gmail.com>
2021-10-03 17:45:03 -0500

    fun with controllers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
   def index
   end
 
+  # the about controller cooresponds with the about page
   def about
+    @about_me = "My name is Andrew Kolbe"
   end
 end
